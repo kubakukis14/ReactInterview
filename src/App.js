7d91602ff04fdc6d02c8a1ac4de80c5d85b9a12d
Jakub Ráček <129507935+kubakukis14@users.noreply.github.com>
2023-05-04 16:04:54 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import { AddNewUser } from "./components/addnewuser";
+import { useEffect, useState } from "react";
+import { AddNewUser } from "./components/AddNewUser";
 import Header from "./components/Header";
-import USERLIST from "./components/UserList";
+import UserList from "./components/UserList";
 import "./styles.css";
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await new Promise((res) => {
+          fetch("https://jsonplaceholder.typicode.com/users") // fetch names from api
+            .then((data) => data.json())
+            .then((usr) => {
+              setUsers(usr);
+              res();
+            });
+        });
+      } catch (err) {
+        alert(err); // alert if fetch fails
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (users) console.log(users); // log out users state when it updates (previosly in form submit function)
+  }, [users]);
+
   return (
     <div className="App">
-      <Header name={"User foo"} />
-      <AddNewUser />
-      <USERLIST />
+      <Header users={users} />
+      <AddNewUser users={users} setUsers={setUsers} />
+      <UserList users={users} setUsers={setUsers} />
     </div>
   );
 }
